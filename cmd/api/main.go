02f2746ffdcfe536c2e2617f1b128e434c2909e9
Vladoskin98 <motov.vladislave@gmail.com>
2025-09-13 00:42:37 +0300

    Chapter 2 is finished

---
+++ cmd/api/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log/slog"
+	"net/http"
+	"os"
+	"time"
+)
+
+const version = "1.0.0"
+
+type config struct {
+	port int
+	env  string //(development, staging, prodution, etc.)
+}
+
+type application struct {
+	config config
+	logger *slog.Logger
+}
+
+func main() {
+	var cfg config
+
+	// Read the value of the port and env command-line flags into the config struct. We
+	// default to using the port number 4000 and the environment "development" if no
+	// corresponding flags are provided.
+	flag.IntVar(&cfg.port, "port", 4000, "API server port")
+	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
+	flag.Parse()
+
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+
+	app := &application{
+		config: cfg,
+		logger: logger,
+	}
+
+	// Declare a HTTP server which listens on the port provided in the config struct,
+	// uses the app.routes as Handler, has some sensible timeout
+	// settings and writes any log messages to the structured logger at Error level.
+	srv := &http.Server{
+		Addr:         fmt.Sprintf(":%d", cfg.port),
+		Handler:      app.routes(),
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		ErrorLog:     slog.NewLogLogger(logger.Handler(), slog.LevelError),
+	}
+
+	logger.Info("starting server", "addr", srv.Addr, "env", cfg.env)
+	err := srv.ListenAndServe()
+	logger.Error(err.Error())
+	os.Exit(1)
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module Vladoskin98/greenlight
+
+go 1.24.6
+
+require github.com/julienschmidt/httprouter v1.3.0 // indirect
